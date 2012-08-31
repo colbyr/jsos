a1fbcc3f2297a539adbfaf58773710a8f60bb13a
Colby Rabideau <crabideau5691@gmail.com>
2012-08-31 15:46:12 -0400

    refactored shell commands

---
+++ scripts/os/Commands.js
@@ -0,0 +1,120 @@
+/**
+ * Shell Commands
+ */
+
+define([
+  'utils/rot13',
+], function (rot13) {
+
+  return {
+    cls: {
+      description: '- Clears the screen and resets the cursor position.',
+      func: function () {
+        _StdIn.clearScreen();
+        _StdIn.resetXY();
+      }
+    },
+
+    help: {
+      description: '- This is the help command. Seek help.',
+      func: function () {
+        _StdIn.putText("Commands:");
+        for (var command in this) {
+          _StdIn.advanceLine();
+          _StdIn.putText(
+            "  " + command +
+            " " + this[command].description
+          );
+        }
+      }
+    },
+
+    man: {
+      description: '<topic> - Displays the MANual page for <topic>',
+      func: function (topic) {
+        if (topic) {
+          switch (topic) {
+            case "help": 
+              _StdIn.putText(
+                "Help displays a list of (hopefully) valid commands."
+              );
+              break;
+            default:
+              _StdIn.putText("No manual entry for " + topic + ".");
+          }
+        } else {
+          _StdIn.putText("Usage: man <topic>  Please supply a topic.");
+        }
+      }
+    },
+
+    prompt: {
+      description: '<string> - Sets the prompt.',
+      func: function (string) {
+        if (string) {
+          _OsShell.promptStr = string;
+        } else {
+          _StdIn.putText("Usage: prompt <string>  Please supply a string.");
+        }
+      }
+    },
+
+    rot13: {
+      description: '<string> - Does rot13 obfuscation on <string>',
+      func: function (string) {
+        if (string) {
+          // Requires utils/rot13 for rot13() function
+          _StdIn.putText(string + " = '" + rot13(string) +"'");
+        } else {
+          _StdIn.putText("Usage: rot13 <string>  Please supply a string.");
+        }
+      }
+    },
+
+    shutdown: {
+      description: '- Shuts down the virtual OS but leaves the underlying hardware simulation running',
+      func: function () {
+         _StdIn.putText("Shutting down...");
+         // Call Kernal shutdown routine.
+        _kernel.shutdown();
+        // TODO: Stop the final prompt from being displayed.  If possible.  Not a high priority.  (Damn OCD!)
+      }
+    },
+
+    trace: {
+      description: '<on | off> - Turns the OS trace on or off.',
+      func: function (toggle) {
+        if (toggle) {
+          switch (toggle) {
+            case "on": 
+              if (_Trace && _SarcasticMode) {
+                _StdIn.putText("Trace is already on, dumbass.");
+              } else {
+                _Trace = true;
+                _StdIn.putText("Trace ON");
+              }
+              break;
+            case "off": 
+              _Trace = false;
+              _StdIn.putText("Trace OFF");
+              break;
+            default:
+              _StdIn.putText("Invalid arguement.  Usage: trace <on | off>.");
+          }
+        } else {
+          _StdIn.putText("Usage: trace <on | off>");
+        }
+      }
+    },
+
+    ver: {
+      description: '- Displays the current version data.',
+      func: function () {
+        _StdIn.putText(APP_NAME + " version " + APP_VERSION);    
+      }
+    }
+    // processes - list the running processes and their IDs
+    // kill <id> - kills the specified process id.
+  };
+
+});
--- scripts/os/shell.js
+++ scripts/os/shell.js
@@ -7,28 +7,29 @@
 // TODO: Write a base class / prototype for system services and let Shell inherit from it.
 
 define([
+  'os/Commands',
   'os/trace',
   'utils/rot13',
   'utils/trim',
   'utils/underscore'
-], function (trace, rot13, trim) {
+], function (Commands, trace, rot13, trim) {
 
   var _kernel = null;
 
   function Shell() {
-    this.promptStr   = ">";
+    this.promptStr = ">";
     this.commandList = [];
-    this.curses      = "[fuvg],[cvff],[shpx],[phag],[pbpxfhpxre],[zbgureshpxre],[gvgf]";
-    this.apologies   = "[sorry]";
+    this.curses = "[fuvg],[cvff],[shpx],[phag],[pbpxfhpxre],[zbgureshpxre],[gvgf]";
+    this.apologies = "[sorry]";
   }
 
   _.extend(Shell.prototype, {
 
-    execute: function (fn, args) {
+    execute: function (func, args) {
       // we just got a command, so advance the line... 
       _StdIn.advanceLine();
       // .. call the command function passing in the args...
-      fn(args);
+      func.apply(Commands, args);
       // Check to see if we need to advance the line again
       if (_StdIn.CurrentXPosition > 0) {
         _StdIn.advanceLine();
@@ -39,35 +40,15 @@ define([
 
     handleInput: function (buffer) {
       trace("Shell Command~" + buffer);
-      // 
       // Parse the input...
-      //
-      var userCommand = new _UserCommand();
-      userCommand = this.parseInput(buffer);
-      // ... and assign the command and args to local variables.
-      var cmd = userCommand.command;
-      var args = userCommand.args;
-      //
-      // Determine the command and execute it.
-      //
-      // Javascript may not support associative arrays (one of the few nice features of PHP, actually)
-      // so we have to iterate over the command list in attempt to find a match.  TODO: Is there a better way?
-      var index = 0;
-      var fn = null;
-      while (!fn && index < this.commandList.length) {
-        if (this.commandList[index].command === cmd) {
-          fn = this.commandList[index].function;
-        } else {
-          ++index;
-        }
-      }
-      if (fn) {
-        this.execute(fn, args);
+      var cmd = new _UserCommand(buffer);
+      if (Commands[cmd.command]) {
+        this.execute(Commands[cmd.command].func, cmd.args);
       } else {
         // It's not found, so check for curses and apologies before declaring the command invalid.
-        if (this.curses.indexOf("[" + rot13(cmd) + "]") >= 0) { // Check for curses.
-          this.execute(shellCurse);
-        } else if (this.apologies.indexOf("[" + cmd + "]") >= 0) { // Check for apoligies.
+        if (this.curses.indexOf("[" + rot13(cmd.toString()) + "]") >= 0) { // Check for curses.
+          this.execute(_curse);
+        } else if (this.apologies.indexOf("[" + cmd.command + "]") >= 0) { // Check for apoligies.
           this.execute(_apology);
         } else { // It's just a bad command.
           this.execute(_invalidCommand);
@@ -77,99 +58,9 @@ define([
 
     // TODO: really hate passing in a ref to the kernel like this
     init: function (kernel) {
-      var sc = null;
       _kernel = kernel;
-      //
-      // Load the command list.
-
-      // ver
-      sc = new _ShellCommand();
-      sc.command = "ver";
-      sc.description = "- Displays the current version data."
-      sc.function = _ver;
-      this.commandList[this.commandList.length] = sc;
-
-      // help
-      sc = new _ShellCommand();
-      sc.command = "help";
-      sc.description = "- This is the help command. Seek help."
-      sc.function = _help;
-      this.commandList[this.commandList.length] = sc;
-
-      // shutdown
-      sc = new _ShellCommand();
-      sc.command = "shutdown";
-      sc.description = "- Shuts down the virtual OS but leaves the underlying hardware simulation running."
-      sc.function = _shutdown;
-      this.commandList[this.commandList.length] = sc;
-
-      // cls
-      sc = new _ShellCommand();
-      sc.command = "cls";
-      sc.description = "- Clears the screen and resets the cursosr position."
-      sc.function = _cls;
-      this.commandList[this.commandList.length] = sc;
-
-      // man <topic>
-      sc = new _ShellCommand();
-      sc.command = "man";
-      sc.description = "<topic> - Displays the MANual page for <topic>.";
-      sc.function = _man;
-      this.commandList[this.commandList.length] = sc;
-
-      // trace <on | off>
-      sc = new _ShellCommand();
-      sc.command = "trace";
-      sc.description = "<on | off> - Turns the OS trace on or off.";
-      sc.function = _trace;
-      this.commandList[this.commandList.length] = sc;
-
-      // rot13 <string>
-      sc = new _ShellCommand();
-      sc.command = "rot13";
-      sc.description = "<string> - Does rot13 obfuscation on <string>.";
-      sc.function = _rot13;
-      this.commandList[this.commandList.length] = sc;
-
-      // prompt <string>
-      sc = new _ShellCommand();
-      sc.command = "prompt";
-      sc.description = "<string> - Sets the prompt.";
-      sc.function = _prompt;
-      this.commandList[this.commandList.length] = sc;
-
-      // processes - list the running processes and their IDs
-      // kill <id> - kills the specified process id.
-
-      //
       // Display the initial prompt.
       this.putPrompt();
-    },
-
-    parseInput: function (buffer) {
-      var retVal = new _UserCommand();
-      //
-      // 1. Remove leading and trailing spaces.
-      buffer = trim(buffer);
-      // 2. Lower-case it.
-      buffer = buffer.toLowerCase();
-      // 3. Separate on spaces so we can determine the command and command-line args, if any.
-      var tempList = buffer.split(" ");
-      // 4. Take the first (zeroth) element and use that as the command.
-      var cmd = tempList.shift();  // Yes, you can do that to an array in Javascript.  See the Queue class.
-      // 4.1 Remove any left-over spaces.
-      cmd = trim(cmd);
-      // 4.2 Record it in the return value.
-      retVal.command = cmd;
-      //
-      // 5. Now create the args array from what's left.
-      for (var i in tempList) {
-        var arg = trim(tempList[i]);
-        if (arg != "") {
-          retVal.args[retVal.args.length] = tempList[i];
-        }
-      }
-      return retVal;
     },
 
     putPrompt: function () {
@@ -198,11 +89,31 @@ define([
   //
   // Another "interior" or "private" class (prototype) used only inside Shell() (we hope).
   //
-  function _UserCommand() {
-    // Properties
-    this.command = "";
-    this.args = [];
+  function _UserCommand(buffer) {
+    this.args = this.parseInput(buffer);
+    this.command = this.args.shift();
   }
+
+  _.extend(_UserCommand.prototype, {
+
+    parseInput: function (string) {
+      return _.reduce(
+        trim(string).toLowerCase().split(" "),
+        function (res, val) {
+          if (val !== '') {
+            res.push(val);
+          }
+          return res;
+        },
+        []
+      );
+    },
+
+    toString: function () {
+      return this.command;
+    }
+
+  });
 
 
   //
@@ -213,24 +124,12 @@ define([
     _SarcasticMode = false;
   }
 
-  function _cls(args) {
-    _StdIn.clearScreen();
-    _StdIn.resetXY();
-  }
 
   function _curse() {
     _StdIn.putText("Oh, so that's how it's going to be, eh? Fine.");
     _StdIn.advanceLine();
     _StdIn.putText("Bitch.");
     _SarcasticMode = true;
-  }
-
-  function _help(args) {
-    _StdIn.putText("Commands:");
-    for (i in _OsShell.commandList) {
-      _StdIn.advanceLine();
-      _StdIn.putText("  " + _OsShell.commandList[i].command + " " + _OsShell.commandList[i].description);
-    }
   }
 
   function _invalidCommand() {
@@ -240,73 +139,6 @@ define([
     } else {
       _StdIn.putText("Type 'help' for, well... help.");
     }
-  }
-
-  function _man(args) {
-    if (args.length > 0) {
-      var topic = args[0];
-      switch (topic) {
-        case "help": 
-          _StdIn.putText("Help displays a list of (hopefully) valid commands.");
-          break;
-        default:
-          _StdIn.putText("No manual entry for " + args[0] + ".");
-      }
-    } else {
-      _StdIn.putText("Usage: man <topic>  Please supply a topic.");
-    }
-  }
-
-  function _prompt(args) {
-    if (args.length > 0) {
-      _OsShell.promptStr = args[0];
-    } else {
-      _StdIn.putText("Usage: prompt <string>  Please supply a string.");
-    }
-  }
-
-  function _rot13(args) {
-    if (args.length > 0) {
-      _StdIn.putText(args[0] + " = '" + rot13(args[0]) +"'");     // Requires Utils.js for rot13() function.
-    } else {
-      _StdIn.putText("Usage: rot13 <string>  Please supply a string.");
-    }
-  }
-
-  function _shutdown(args) {
-     _StdIn.putText("Shutting down...");
-     // Call Kernal shutdown routine.
-    _kernel.shutdown();   
-    // TODO: Stop the final prompt from being displayed.  If possible.  Not a high priority.  (Damn OCD!)
-  }
-
-  function _trace(args)
-  {
-    if (args.length > 0) {
-      var setting = args[0];
-      switch (setting) {
-        case "on": 
-          if (_Trace && _SarcasticMode) {
-            _StdIn.putText("Trace is already on, dumbass.");
-          } else {
-            _Trace = true;
-            _StdIn.putText("Trace ON");
-          }
-          break;
-        case "off": 
-          _Trace = false;
-          _StdIn.putText("Trace OFF");
-          break;
-        default:
-          _StdIn.putText("Invalid arguement.  Usage: trace <on | off>.");
-      }
-    } else {
-      _StdIn.putText("Usage: trace <on | off>");
-    }
-  }
-
-  function _ver(args) {
-    _StdIn.putText(APP_NAME + " version " + APP_VERSION);    
   }
 
   return Shell;
