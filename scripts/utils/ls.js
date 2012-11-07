58bfd37561488e0da48ca2e910a60a7202b5a6f6
Colby Rabideau <crabideau5691@gmail.com>
2012-11-07 14:56:55 -0500

    created localStorage wrapper

---
+++ scripts/utils/ls.js
@@ -0,0 +1,92 @@
+define([], function () {
+
+  return {
+
+    /**
+     * Removes all values in LS
+     *
+     * @return object
+     */
+    clear: function () {
+      window.localStorage.clear();
+      return this;
+    },
+
+    /**
+     * Returns true if key is set
+     *
+     * @param  string
+     * @return bool
+     */
+    contains: function (key) {
+      return window.localStorage.hasOwnProperty(key);
+    },
+
+    /**
+     * Returns a key's value
+     *
+     * @param  string
+     * @return string
+     */
+    get: function (key) {
+      return window.localStorage.getItem(key);
+    },
+
+    /**
+     * Returns the key of a value in LS
+     *
+     * @param  string
+     * @return string
+     */
+    getKey: function (value) {
+      return window.localStorage.key(value);
+    },
+
+    /**
+     * Returns an array of all keys in LS
+     *
+     * @return array
+     */
+    keys: function () {
+      var keys = [];
+      for (var k in window.localStorage) {
+        keys.push(k);
+      }
+      return keys;
+    },
+
+    /**
+     * Remove a key-value pair from LS
+     *
+     * @param  string
+     * @return object
+     */
+    remove: function (key) {
+      window.localStorage.removeItem(key);
+      return this;
+    },
+
+    /**
+     * Set a key-value pair
+     *
+     * @param  string
+     * @param  string
+     * @return object
+     */
+    set: function (key, value) {
+      window.localStorage.setItem(key, value);
+      return this;
+    },
+
+    /**
+     * Returns true if the browser supports LS
+     *
+     * @return bool
+     */
+    isSupported: function () {
+      return !!window.localStorage;
+    }
+
+  };
+
+});
